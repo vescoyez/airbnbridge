01e5253431ea1fa9e03da8b432882f3cdc2374b7
Adrien van den Branden de Reeth <avdbranden@gmail.com>
2015-12-03 14:35:58 +0100

    view index, show & new, no links yet

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151202171728) do
+ActiveRecord::Schema.define(version: 20151203095436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,16 @@ ActiveRecord::Schema.define(version: 20151202171728) do
 
   add_index "bridges", ["user_id"], name: "index_bridges_on_user_id", using: :btree
 
+  create_table "comments", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "content"
+    t.integer  "booking_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["booking_id"], name: "index_comments_on_booking_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -74,4 +84,5 @@ ActiveRecord::Schema.define(version: 20151202171728) do
   add_foreign_key "bookings", "bridges"
   add_foreign_key "bookings", "users"
   add_foreign_key "bridges", "users"
+  add_foreign_key "comments", "bookings"
 end
