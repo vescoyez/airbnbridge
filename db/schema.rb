0278880c0ed6475342d883738fd6ab186688a2f6
Adrien van den Branden de Reeth <avdbranden@gmail.com>
2015-12-04 15:29:17 +0100

    finalising bridges pictures

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203095436) do
+ActiveRecord::Schema.define(version: 20151204110856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,11 +37,15 @@ ActiveRecord::Schema.define(version: 20151203095436) do
     t.integer  "capacity"
     t.integer  "user_id"
     t.string   "address"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
     t.float    "price"
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
   end
 
   add_index "bridges", ["user_id"], name: "index_bridges_on_user_id", using: :btree
